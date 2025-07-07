c650dbcba51281000413ca8e4f3504495faf61a0
pupbrained <mars@pupbrained.xyz>
2025-07-07 03:29:04 -0400

    ?

--- config/lang/nix.nix
+++ config/lang/nix.nix
@@ -1,16 +1,10 @@
 {...}: {
   plugins = {
     lsp.servers = {
-      nixd = {
-        enable = true;
-        packageFallback = true;
-      };
-
-      statix = {
-        enable = true;
-        packageFallback = true;
-      };
+      nixd.enable = true;
+      statix.enable = true;
     };
+
     conform-nvim.settings.formatters_by_ft.nix = ["alejandra"];
   };
 }
