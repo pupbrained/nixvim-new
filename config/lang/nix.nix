102b5b637269b7c19e1a99b7093699af223c7870
pupbrained <mars@pupbrained.xyz>
2025-07-07 03:25:05 -0400

    add statix

--- config/lang/nix.nix
+++ config/lang/nix.nix
@@ -1,6 +1,16 @@
 {...}: {
   plugins = {
-    lsp.servers.nixd.enable = true;
+    lsp.servers = {
+      nixd = {
+        enable = true;
+        packageFallback = true;
+      };
+
+      statix = {
+        enable = true;
+        packageFallback = true;
+      };
+    };
     conform-nvim.settings.formatters_by_ft.nix = ["alejandra"];
   };
 }
